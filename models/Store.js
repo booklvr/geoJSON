ec4618bd87c4b3d9310236d9bae3ed280c8793b3
nick <nick.a.dewaal@gmail.com>
2020-01-03 16:39:22 +0900

    solving map api problem

--- models/Store.js
+++ models/Store.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 const geocoder = require('../utils/geocoder');
 
 const StoreSchema = new mongoose.Schema({
-    storeID: {
+    storeId: {
         type: String,
-        required: [true, 'Please add a store ID'],
+        required: [true, 'Please add a store Id'],
         unique: true,
         trim: true,
         maxlength: [10, 'Store ID must be less than 10 chars']
--- routes/stores.js
+++ routes/stores.js
@@ -3,7 +3,10 @@ const { getStores, addStore } = require('../controllers/stores')
 
 const router = express.Router();
 
-router.route('/').get(getStores).post(addStore);
+router
+    .route('/')
+    .get(getStores)
+    .post(addStore);
 
 
 
